f5255374689e18111688c7f136dc6293cfc2ddd1
SergeyPichugin <8490839@mail.ru>
2024-07-29 17:22:47 +0300

    gh pages

--- src/components/Layout/Layout.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./Layout.module.scss";
-
-const Layout = ({ ...props }) => {
-  const { children } = props;
-  return <div className={styles.layout}>{children}</div>;
-};
-
-export default Layout;
--- src/components/SwitchTheme/SwitchTheme.tsx
+++
@@ -1,4 +0,0 @@
-const SwitchTheme = () => {
-  return <div>-</div>;
-};
-export default SwitchTheme;
